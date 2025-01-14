17dc114605df2b96123fbc77ce345a6a45c02745
ItzThaMr <rooversmax@hotmail.com>
2025-01-14 19:13:32 +0100

    added fps counter

--- main.py
+++ main.py
@@ -7,12 +7,19 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fps = pygame.time.Clock()
+    dt = 0
+
+
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = fps.tick(60) / 1000
             
-        screen.fill((0,0,0))
+        screen.fill(("black"))
         pygame.display.flip()
 
 
